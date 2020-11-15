46eb0b614468b09469a39a0cf358ff61f8936f07
YiranWan <wanyiran_0223@163.com>
2020-11-15 15:50:49 +0800

    Initial Commit

---
+++ instagramy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagramy
+//
+//  Created by 万亦然 on 2020/11/15.
+//  Copyright © 2020 Maraschino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
